e5521a8b106ee14b3d380e2122a55600295a507c
Dominik <dziamski@gmk.de>
2019-01-12 23:41:44 +0100

    newDesign

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,56 @@
+window.onload = function welcomeSize () {
+    var home = document.getElementById("Home");
+    var welcomescreen = document.getElementById("welcome");
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    welcomescreen.style.backgroundColor = '#ececec';
+    welcomescreen.style.height = height + 'px';
+    welcomescreen.style.width = width + 'px';
+    home.style.height = height + 'px';
+
+
+    var logo = document.getElementById('logo_start');
+    logo.style.height = height / 2 +'px';
+    logo.style.display = 'block';
+
+    var logoWidth = logo.offsetWidth;
+
+    var wrap = document.getElementById('wrap');
+    wrap.style.paddingTop = height / 4 + 'px';
+    wrap.style.paddingLeft = (width / 2) - logoWidth / 2 + 'px';
+};
+
+window.onresize = function homeSite (){
+  var home = document.getElementById("Home");
+  var height = window.innerHeight;
+  var width = window.innerWidth;
+  home.style.height = height + 'px';
+ }
+
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+
+var opacity = 0;
+function logodelay(){
+  if (opacity < 10) {
+    opacity += 1;
+    setTimeout(function(){logodelay()},50);
+  }
+  document.getElementById('logo_start').style.opacity = opacity / 10;
+
+  if (opacity == 10){
+    setTimeout(function(){document.getElementById('Home').scrollIntoView({behavior: 'smooth'});},1000);
+  };
+
+  setTimeout(function(){document.getElementById('welcome').style.display = 'none';},2000);
+}
+
+
+
+delay(function(){logodelay()},500);
