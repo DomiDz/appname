8c85c279368b25939aceda6c05b4d34d70f427b0
Dominik <dziamski@gmk.de>
2019-01-21 13:49:07 +0100

    product edit-site added price and layout

--- db/migrate/20190117142422_rename_color_in_products.rb
+++
@@ -1,5 +0,0 @@
-class RenameColorInProducts < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :products, :color, :colour
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_142422) do
+ActiveRecord::Schema.define(version: 2019_01_21_122938) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 2019_01_17_142422) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "colour"
+    t.string "location"
+    t.string "price"
   end
 
 end
