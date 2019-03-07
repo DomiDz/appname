9c934789a23cec87d1e4d09b286c2153f398997c
Dominik <dziamski@gmk.de>
2019-03-07 17:45:35 +0100

    sendgrid implemended

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -20,3 +20,15 @@ class SimplePagesController < ApplicationController
     redirect_to "/simple_pages/photography"
   end
 end
+
+class SimplePagesController < ApplicationController
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'mail@d-dziamski.de',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+end
