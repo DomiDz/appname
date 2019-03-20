dba0a1df0171a3e6636c993baabc6a10a7ab6471
Dominik <dziamski@gmk.de>
2019-03-20 23:32:26 +0100

    Admin functions added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
@@ -12,14 +14,18 @@ class CommentsController < ApplicationController
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
-  
-  def destroy
   end
+  def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
+  end
+    
 
   private
 
     def comment_params
       params.require(:comment).permit(:user_id, :body, :rating)
     end
-end
 end
