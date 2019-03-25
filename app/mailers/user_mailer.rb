ad63ece27159643e77db3b3f9a1ece9eb799d204
Dominik <dziamski@gmk.de>
2019-03-25 21:34:00 +0100

    bug fixes - Mail upon sign_in

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,12 @@ class UserMailer < ApplicationMailer
          to: 'your-email@example.com',
          subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "for Senses"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
+
 end
+
