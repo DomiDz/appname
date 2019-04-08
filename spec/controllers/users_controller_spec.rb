8670da6ba616f1ca69f761c4c0ca988cfe5ab85e
Dominik <dziamski@gmk.de>
2019-04-08 17:08:25 +0200

    6.4 - bugs fixed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,7 @@ describe UsersController, type: :controller do
  #let(:user) {User.create!(first_name: "testUserA", email: "testA@test.com", password: "123456")}
   #  let(:userB) {User.create!(first_name: "testUser", email: "testB@test.com", password: "123456")}
   before do 
-   @user1= FactoryBot.create(:user)
+   @user= FactoryBot.create(:user)
    @user2= FactoryBot.create(:user)
   end
 
@@ -16,12 +16,12 @@ describe UsersController, type: :controller do
     context "user is logged in" do
 
       before do
-        sign_in @user1
+        sign_in @user
       end
 
       it "loads correct user profile" do
-        get :show, params: {id: @user1.id}
-        expect(assigns(@user1)).to eq @user1
+        get :show, params: {id: @user.id}
+        expect(assigns(:user)).to eq @user
       end
 
       it 'cannot load other user profile' do
@@ -33,7 +33,7 @@ describe UsersController, type: :controller do
 
     context 'when a user is not logged in' do
         it 'redirects to root' do
-        get :show, params: { id: @user1.id }
+        get :show, params: { id: @user.id }
         expect(response).to redirect_to(root_path)
     end
 
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
+    
     factory :user do
         email
         password "1234567890"
@@ -7,4 +8,13 @@ FactoryBot.define do
         last_name "Example"
         admin false
     end
+
+    factory :admin, class: User do
+        email 
+        password "qwertyuiop"
+        first_name "Admin"
+        last_name "User"
+        admin true
+      end
+
 end
