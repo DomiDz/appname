9e82e9af40b66169ebc27a46a9284bf378391bd0
Dominik <dziamski@gmk.de>
2019-04-10 23:25:57 +0200

    bug

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'payments/thank_you'
+  get 'payments/create'
   root 'simple_pages#index'
   post 'simple_pages/thank_you'
   post 'payments/create'
