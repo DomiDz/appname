d2a5fa09ce876b1bef2c92b5c76329608e18ee75
Dominik <dziamski@gmk.de>
2019-04-26 15:11:46 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,3 @@ Rails.application.configure do
 end
 
 
-config.cache_store = :mem_cache_store,
-                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                    {:username => ENV["MEMCACHIER_USERNAME"],
-                     :password => ENV["MEMCACHIER_PASSWORD"],
-                     :failover => true,
-                     :socket_timeout => 1.5,
-                     :socket_failure_delay => 0.2,
-                     :down_retry_delay => 60
-                    }
