0d08bbfedac206db283926a8f468b26c126eb2fd
Dominik <dziamski@gmk.de>
2019-05-01 16:40:49 +0200

    rewritten code for readability

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -7,7 +7,7 @@ class CommentUpdateJob < ApplicationJob
   
   private
   
-  def render_comment(comment, current_user)
-    CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
-  end
+    def render_comment(comment, current_user)
+      CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
+    end
 end
